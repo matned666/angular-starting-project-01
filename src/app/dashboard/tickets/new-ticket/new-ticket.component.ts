5de4ba72f2f018410a1ec4c83f915062d1b73e03
MNiedbal <mniedbal@sygnity.pl>
2024-11-19 11:42:56 +0100

    init

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {SubmitButtonComponent} from "../../../submit-button/submit-button.component";
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+    imports: [
+        SubmitButtonComponent
+    ],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css'
+})
+export class NewTicketComponent {
+
+}
