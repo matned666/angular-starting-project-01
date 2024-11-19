cd98eb9ff366d6b99673a4aed6cf451c1dc594d4
MNiedbal <mniedbal@sygnity.pl>
2024-11-20 00:51:24 +0100

    afterContentInit - safe access to the @ChildView elements

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, viewChild, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild} from '@angular/core';
 import {ButtonComponent} from "../../../shared/submit-button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
 import {FormsModule} from "@angular/forms";
@@ -14,9 +14,17 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
   // @ViewChild('form') form? : ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(): void {
+    console.log("ON INIT");
+  }
+
+  ngAfterViewInit() {
+    console.log("AFTER VIEW INIT");
+  }
 
 
   onSubmit(titleInput: string, requestInput: string) {
