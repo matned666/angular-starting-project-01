8afde060306b067add068babb7792275e529d171
MNiedbal <mniedbal@sygnity.pl>
2024-11-19 16:05:36 +0100

    template variable ... ex. #title

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from "../../../shared/submit-button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+  onSubmit(titleInput: HTMLInputElement, requestInput: HTMLTextAreaElement) {
+    const enteredTitle = titleInput.value;
+
+    console.log(titleInput);
+    console.log('req:' + requestInput.value);
+  }
 }
